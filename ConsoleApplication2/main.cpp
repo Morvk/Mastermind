462462b09d2e5789ba0ea658d6e78747a88eadf9
Tomasz Morawski <56163214+Morvk@users.noreply.github.com>
2023-02-07 21:24:56 +0100

    Add files via upload

---
+++ ConsoleApplication2/main.cpp
@@ -0,0 +1,198 @@
+﻿#include <iostream>
+#include "plansza.h"
+using namespace std;
+
+//zmienne do przekazywania danych użytkownika do plansza.h w celu sprawdzenia szyfru
+//używając szyfru(cipher) zamiast kodu(code), ponieważ jest to mniej mylące
+char a;
+char b;
+char c;
+char d;
+
+bool winFlag = false;
+
+void startUp();
+
+int main() {
+   
+    //generuje obiekt tablicy z pliku plansza.h i klasy tablicy
+    board boardObject;
+    //generowanie szyfru do gry
+    boardObject.genCipher();
+  
+    startUp();
+
+    //instrukcje gry
+    cout << endl << endl << "Welcome to MASTERMIND";
+
+    //ustawienie vars na stan domyślny, głównie do resetowania powtórek
+    a = 'O';
+    b = 'O';
+    c = 'O';
+    d = 'O';
+    
+    cout << endl << endl << "Here is your game board" << endl << endl;
+
+    //tworzenie typu planszy z wartościami znaków do wykorzystania w grze
+    // inicjuje z 'O', aby reprezentować puste miejsca na "pegu"
+    boardObject.boardBuilder(a, b, c, d);
+
+  //główna pętla gry
+  //--------------------------------------------------------//
+  //--------------------------------------------------------//
+    for (int n = 1; n < 11; n++) {
+
+        //Wyjaśnienie, w jaki sposób użytkownik może wprowadzać swoje domysły do ​​gry
+        cout << "\nEnter a letter to guess: (r)ed, (o)range, (y)ellow, (g)reen, (b)lue, (p)urple\n";
+        cout << "Or type 'c' to check your number of guesses left\n\n";
+
+        char input;
+        bool inputFlag = true;
+
+
+        //for pętla dla każdego z czterech wejść dla szyfru
+        for (int i = 0; i < 4; i++) {
+            //do pętla do działania w nieskończoność, aż do poprawnego wprowadzenia danych
+            do {
+                cin >> input;
+                //założenie że dane wejsciowe są prawidłowe
+                inputFlag = true;
+
+                //sprawdzanie, czy dane wejściowe mają znak, który odpowiada kolorowi
+                if (input == 'r' || input == 'o' || input == 'y'
+                    || input == 'g' || input == 'b' || input == 'p'
+                    || input == 'c' || input == 'C') {
+                    break;
+                }
+
+                //pasywno-agresywny przekaz dla osób, które łamią zasady
+                cout << "Please enter a character respective of a color ONLY" << endl;
+                cin.clear();
+                cin.ignore(10000, '\n');
+
+                //brak prawdziwego warunku końcowego, ponieważ inputFlag jest zawsze prawdziwy
+                //jedynym sposobem zakończenia jest aktywacja instrukcji if i przerwanie;
+            } while (inputFlag == true);
+
+            if (input == 'c' || input == 'C') {
+                cout << "Your number of guesses left is ";
+                boardObject.display();
+                i = i - 1;
+                cout << "\nEnter a letter to guess: (R)ed, (O)range, (Y)ellow, (G)reen, (B)lue, (P)urple\n";
+                cout << "Or type 'c' to check your number of guesses left\n\n";
+                continue;
+            }
+
+            //if w celu przypomnienia użytkownikowi, aby kontynuował wprowadzanie do czterech wejść
+            if (i != 3) {
+                cout << endl << "Thanks, please enter another character" << endl;
+            }
+            cin.clear();
+            cin.ignore(10000, '\n');
+
+            //ustawia dane wejściowe na rzeczywiste wartości dla obiektu planszy podczas jego działania
+            if (i == 0) {
+                a = input;
+            }if (i == 1) {
+                b = input;
+            }if (i == 2) {
+                c = input;
+            }if (i == 3) {
+                d = input;
+            }
+
+        }
+
+        //pozwala użytkownikowi kontynuować, gdy jest gotowy i usuwa planszę z wyborów w tej rundzie
+        cout << endl;
+        system("read -p 'Press Enter to continue...' var");
+        cout << endl << endl;
+        
+
+        //wysyła wybrane przez użytkownika wartości do sprawdzenia w klasie tablicy
+        boardObject.boardBuilder(a, b, c, d);
+
+        cout << endl;
+
+        //po wygenerowaniu planszy sprawdza warunek wygranej
+        //w przypadku wygranej, wyświetlony kod i zamknięcie systemu
+
+        if (boardObject.checkWin() == true) {
+            cout << "Congratulations! YOU WIN!" << endl;
+            cout << "You successfully cracked the code!" << endl;
+            boardObject.revealCode();
+            cout << endl;
+            winFlag = true;
+            break;
+        }
+
+        //używa przeciążonego operatora
+        --boardObject;
+
+        //używa funkcji wyświetlania dla przeciążonego operatora do
+        //pokazania pozostałych prób
+        cout << "You have ";
+        boardObject.display();
+        cout << "Guesses remaining" << endl;
+
+    }
+
+    //pozwala użytkownikowi na ponowną rozgrywkę jeśli przegrał
+    char again;
+
+    if (winFlag == false) {//---------
+        //wyswietla gdy przegra sie gre
+        cout << "Sorry thats the max number of guesses..." << endl << endl;
+        cout << "YOU LOSE!" << endl << endl;
+
+        //ujawnia kod za pomocą metody w klasie plansza
+        //nie może uzyskać bezpośredniego dostępu do cipher[], ponieważ jest to prywatna tablica znaków
+        boardObject.revealCode();
+        cout << endl << endl << endl;
+    }//---------
+
+    //pozwala użytkownikowi zresetować grę i spróbować ponownie lub całkowicie wyjść z systemu
+    cout << endl;
+    system("echo 'Press Enter to continue...'");
+    cout << "Play Again? (y/n)" << endl;
+
+    //skopiowane z góry, nie zezwala żadnemu użytkownikowi na zabawne interesy
+    //dane wejściowe użytkownika, dopuszcza tylko char y lub n
+    bool inputFlagEnd = true;
+    do {
+        cin >> again;
+        //załóżenie, że dane wejściowe są prawidłowe
+        inputFlagEnd = true;
+
+        //sprawdza, czy dane wejściowe mają znak, który odpowiada kolorowi
+        if (again == 'y' || again == 'Y'
+            || again == 'n' || again == 'N')
+        {
+            if (again == 'y' || again == 'Y') {
+                main();
+            }
+            else if (again == 'n' || again == 'N') {
+                exit(0);
+            }
+
+        }
+        //pasywno - agresywny przekaz dla osób, które łamią zasady
+        cout << "Please enter correct input (y/n)" << endl;
+        cin.clear();
+        cin.ignore(10000, '\n');
+        //brak prawdziwego warunku końcowego, ponieważ inputFlag jest zawsze prawdziwy
+        //jedynym sposobem zakończenia jest aktywacja instrukcji if i przerwanie;
+    } while (inputFlagEnd == true);
+    //koniec main
+}
+
+//napis mastermind dla lepszego wygladu
+void startUp() {
+    cout << " __  __           _                      _           _ " << endl;
+    cout << "|  \\/  | __ _ ___| |_ ___ _ __ _ __ ___ (_)_ __   __| |" << endl;
+    cout << "| |\\/| |/ _` / __| __/ _ \\ '__| '_ ` _ \\| | '_ \\ / _` |" << endl;
+    cout << "| |  | | (_| \\__ \\ ||  __/ |  | | | | | | | | | | (_| |" << endl;
+    cout << "|_|  |_|\\__,_|___/\\__\\___|_|  |_| |_| |_|_|_| |_|\\__,_|" << endl;
+    cout << "-------------------------------------------------------";
+
+}
---
+++ ConsoleApplication2/plansza.h
@@ -0,0 +1,243 @@
+using namespace std;
+#include <iostream>
+#include <ctime>
+#include <string>
+
+class board {
+
+    //prywatne dane takie jak podpowiedzi oraz tablica kodu
+private:
+    char w, x, y, z;
+    char cipher[4];
+    char tempArray[4];
+    char tempCipher[4];
+    bool toggleDuplicates = false;
+    char firstPos, secondPos, thirdPos, fourthPos; //pozycje
+    int count; //licznik
+    int blackCounter;//licznik dobrych znaków na dobrych pozycjach
+    int whiteCounter;//licznik dobrych znaków na z³ych pozycjach
+
+
+public:
+
+    board() : count(10) {} //10 prób zgadniecia kolorow
+    void operator -- () {
+
+        --count;
+
+    }
+
+    void display() {
+        cout << count << endl;
+    }
+    //------------------------------------//
+
+
+    void boardBuilder(char firstPos, char secondPos, char thirdPos, char fourthPos) {
+        this->firstPos = firstPos;
+        this->secondPos = secondPos;
+        this->thirdPos = thirdPos;
+        this->fourthPos = fourthPos; //wpisanie liter koloru
+
+       
+        determineHints();//odpowiada za podpowiedzi na dan¹ runde
+
+        
+        genBoardLine(); //tworzenie lini do ³adniejszego wyœwietlania
+        cout << "| " << firstPos << " | " << secondPos
+            << " | " << thirdPos << " | " << fourthPos << " | ";
+        cout << w << " | " << x << " | " <<
+            y << " | " << z << " |" << endl;
+        genBoardLine();//zamkniecie rundy
+
+        cout << endl << "White Pegs: " << whiteCounter;
+        cout << endl << "Black Pegs: " << blackCounter;
+        cout << endl;//wyswietla nowe podpowiedzi
+        return;
+    };
+
+    void determineHints() { //metoda usatlajaca podpowiedzi dla uzytkownika
+        //pocz¹tkowo ustawia wszystkie "pegi" do stanu domyslnego oraz nadpisuje jak jest to potrzebne
+        w = 'X';
+        x = 'X';
+        y = 'X';
+        z = 'X';
+
+        //licznik "pegów"
+        blackCounter = 0;
+        whiteCounter = 0;
+
+        //wstawienie wybranych przez gracza kolorow do tymczasowego arraya
+        tempArray[0] = firstPos;
+        tempArray[1] = secondPos;
+        tempArray[2] = thirdPos;
+        tempArray[3] = fourthPos;
+
+        //wstawienie wybranych przez gracza kolorow do tymczasowego arraya
+        tempCipher[0] = cipher[0];
+        tempCipher[1] = cipher[1];
+        tempCipher[2] = cipher[2];
+        tempCipher[3] = cipher[3];
+
+        //tymczasowe arraye stworzone by mo¿na by³o nadpisywac dane
+        //podczas obliczania bia³ych i czarnych "pegów"
+
+        // Upewniienie siê, ¿e "pegi" nie s¹ liczone podwójnie,
+        // wiêcej ni¿ jeden "peg" tego samego koloru,
+        // sprawdzenie, czy znaleziono jakieœ prawid³owe kolory nie na miejscu
+
+        //zagnie¿d¿ona pêtla for w celu okreœlenia zarówno czarnych, jak i bia³ych "pegów"
+        for (int i = 0; i < 4; i++) {
+
+            // liczenie czarnych "pegów" na podstawie tego, czy dane wprowadzone przez u¿ytkownika pasuj¹ do kodu
+            // dok³adnie w tej samej pozycji i w tablicach o tym samym rozmiarze
+            if (tempArray[i] == tempCipher[i]) {
+                blackCounter++;
+
+                //po policzeniu zamieniaj¹ siê w nonsens, zapewnia to ¿e nie zostan¹ policzone jeszcze raz
+                //zliczone ponownie przez wykrywacz bia³ych "pegów"
+                tempArray[i] = 'Q';
+                tempCipher[i] = 'Z';
+            }
+            else {
+
+                //szukanie bia³ych "pegów"
+                //checks if position is wrong but color is correct
+                for (int m = 0; m < 4; m++) {
+                    if (tempArray[i] == tempCipher[m] &&
+                        tempArray[m] != tempCipher[m]) {
+                        whiteCounter++;
+
+                        //po policzeniu zamieniaj¹ siê w nonsens, zapewnia to ¿e nie zostan¹ policzone jeszcze raz
+                        //zliczone ponownie przez wykrywacz bia³ych "pegów"
+                        tempCipher[m] = 'Q';
+                        tempArray[i] = 'Z';
+                        break;
+                    }
+                }
+
+            }
+
+        }
+
+
+        //nadpisuje domyœlne "pegi" kolorem bia³ym dla ka¿dego obliczonego bia³ego "pega"
+
+        //bia³e "pegi" wyswietlane sa z prawej do lewej
+        if (whiteCounter > 0) {
+            z = 'W';
+        }
+        if (whiteCounter > 1) {
+            y = 'W';
+        }
+        if (whiteCounter > 2) {
+            x = 'W';
+        }
+        if (whiteCounter > 3) {
+            w = 'W';
+        }
+
+        //czarne "pegi" maj¹ pierwszeñstwo przed bia³ymi "pegami", a czarny mo¿e byæ prawdziwy
+        //dla jednego, podczas gdy bia³y jest równie¿ prawdziwy, wiêc drukowanie na bia³ym jest przydatne
+        //czarne "pegi" wyswietlane sa z lewej do prawej
+        if (blackCounter > 0) {
+            w = 'B';
+        }
+        if (blackCounter > 1) {
+            x = 'B';
+        }
+        if (blackCounter > 2) {
+            y = 'B';
+        }
+        if (blackCounter > 3) {
+            z = 'B';
+        }
+
+    };
+    //koniec metody wyznaczania podpowiedzi
+
+
+    //metoda sprawdzania wygranej przez sprawdzenie, czy ka¿da pozycja odpowiada pozycji w szyfrze
+    bool checkWin() {
+        if (firstPos == cipher[0] && secondPos == cipher[1] && thirdPos == cipher[2] && fourthPos == cipher[3]) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    //ogólna metoda linii planszy omówiona powy¿ej
+    void genBoardLine() {
+        cout << "|---------------|---------------|" << endl;
+        return;
+    };
+
+
+    //generuje losowe liczby dla kodu za pomoc¹ ctime
+    //ka¿da liczba ca³kowita 1 - 6 odpowiada mo¿liwemu kolorowi
+    void genCipher() {
+
+        int num;
+        srand(time(0));
+
+        for (int i = 0; i < 4; i++) {
+
+            num = rand() % 6 + 1;
+
+            if (num == 1) {
+                cipher[i] = 'b';
+            }
+            else if (num == 2) {
+                cipher[i] = 'g';
+            }
+            else if (num == 3) {
+                cipher[i] = 'o';
+            }
+            else if (num == 4) {
+                cipher[i] = 'p';
+            }
+            else if (num == 5) {
+                cipher[i] = 'r';
+            }
+            else if (num == 6) {
+                cipher[i] = 'y';
+            }
+        }
+
+        //DEBUG
+        //sluzy do wyawietlenia szyfru na poczatku gry, uzywane do testow
+      //  cout << endl << "DEBUG" << endl;
+       // for (int i = 0; i < 4; i++) {
+      //      cout << cipher[i];
+      //  }
+
+        return;
+    };
+
+
+    //metoda ujawniania kodu zarówno dla warunków wygranej, jak i przegranej
+    //resetuje równie¿ wartoœci "pegów" podpowiedzi do powtórki od tego czasu
+    //nie mo¿na tego zrobiæ z main.cpp
+    //nie ma jednak wp³ywu na warunek wygranej
+    void revealCode() {
+
+        cout << endl << "The code was..." << endl << endl;
+
+        for (int i = 0; i < 4; i++) {
+
+            cout << (cipher[i]);
+
+        }
+
+        //ustawia "pegi" po prawej stronie w domyœlnym stanie do nastêpnej gry
+        //mo¿na to zrobiæ tylko w tej klasie
+        w = 'X';
+        x = 'X';
+        y = 'X';
+        z = 'X';
+
+        return;
+    }
+    //koniec klasy plansza
+};
